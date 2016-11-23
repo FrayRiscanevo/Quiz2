90edc021bb0aac9738263857204721c39c03dc41
USER <USER@Z97-PDesktopW10>
2016-11-23 18:39:47 -0500

    Quiz 2

---
+++ src/quiz/pkg2/Game.java
@@ -0,0 +1,36 @@
+package quiz.pkg2;
+
+public class Game {
+    private String title, description, author;
+
+    public Game(String title, String description, String author) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+}

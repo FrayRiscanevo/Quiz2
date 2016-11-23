4f23271c557c0561bc8090285ba201ec38ff00d3
USER <USER@Z97-PDesktopW10>
2016-11-23 18:40:35 -0500

    Quiz 2

---
+++ src/quiz/pkg2/Screen.java
@@ -0,0 +1,54 @@
+package quiz.pkg2;
+
+public class Screen {
+    private int width, height, pixelBlock, blockX, blockY;
+
+    public Screen(int width, int height, int pixelBlock, int blockX, int blockY) {
+        this.width = width;
+        this.height = height;
+        this.pixelBlock = pixelBlock;
+        this.blockX = blockX;
+        this.blockY = blockY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getPixelBlock() {
+        return pixelBlock;
+    }
+
+    public void setPixelBlock(int pixelBlock) {
+        this.pixelBlock = pixelBlock;
+    }
+
+    public int getBlockX() {
+        return blockX;
+    }
+
+    public void setBlockX(int blockX) {
+        this.blockX = blockX;
+    }
+
+    public int getBlockY() {
+        return blockY;
+    }
+
+    public void setBlockY(int blockY) {
+        this.blockY = blockY;
+    }
+    
+}

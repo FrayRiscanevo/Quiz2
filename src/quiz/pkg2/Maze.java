ff1e87755ea68b4df9f4d2bbee0f16ec34f91b1d
USER <USER@Z97-PDesktopW10>
2016-11-23 18:40:16 -0500

    Quiz 2

---
+++ src/quiz/pkg2/Maze.java
@@ -0,0 +1,14 @@
+package quiz.pkg2;
+
+public class Maze extends Game{
+    private int time;
+    public Screen configureScree= new Screen();
+    public Hero heroes= new Hero();
+    public World worlds= new World();
+
+    public Maze(int time, String title, String description, String author) {
+        super(title, description, author);
+        this.time = time;
+    }
+ 
+}
